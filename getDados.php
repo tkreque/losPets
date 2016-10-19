86605e70f721d9bdd260678f27ba60322491d727
Thiago Klein Reque <blindreque@gmail.com>
2016-10-19 23:07:50 +0000

    modified:   getDados.php 	new file:   getTag.php 	modified:   index.html 	new file:   index2.html

--- getDados.php
+++ getDados.php
@@ -1,10 +1,10 @@
 <?php
     //Conectando ao banco de dados
-    $con = new mysqli("localhost", "pets", "123", "gMaps");
+    $con = new mysqli("localhost", "pets", "123", "pets");
     if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
     
     //Consultando banco de dados
-    $qryLista = mysqli_query($con, "SELECT * FROM geoLoc");    
+    $qryLista = mysqli_query($con, "SELECT * FROM animal");    
     while($resultado = mysqli_fetch_assoc($qryLista)){
         $vetor[] = array_map('utf8_encode', $resultado); 
     }    
---
+++ getTag.php
@@ -0,0 +1,28 @@
+<?php
+    //Conectando ao banco de dados
+    $con = new mysqli("localhost", "pets", "123", "pets");
+    if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
+	
+	$tag = $_POST["tag"];
+	
+	$SQL = "SELECT ani_latitude , ani_longitude FROM animal
+	INNER JOIN raca ON animal.ani_raca = raca.raca_id
+
+	INNER JOIN usuario ON animal.ani_usuario = usuario.user_id
+	INNER JOIN animal_tag ON animal.ani_id = animal_tag.anitag_animal
+	INNER JOIN tag ON animal_tag.anitag_tag = tag.tag_id
+	AND UPPER(tag.tag_tag) LIKE UPPER('%".$tag."%')";
+	
+	
+    
+    //Consultando banco de dados
+    $qryLista = mysqli_query($con, $SQL);    
+	//$qryLista = mysqli_query($con, "select ani_latitude , ani_longitude from animal");   
+    while($resultado = mysqli_fetch_assoc($qryLista)){
+        $vetor[] = array_map('utf8_encode', $resultado); 
+    }    
+    
+    //Passando vetor em forma de json
+    echo json_encode($vetor);
+    
+?>
