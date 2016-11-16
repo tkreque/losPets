68684c2c21f1deff1135d0698dabe5c35bcd62e4
Thiago Klein Reque <blindreque@gmail.com>
2016-11-16 23:24:44 +0000

    modified:   getAnimais.php 	modified:   getDados.php 	modified:   getTag.php 	new file:   img/an.jpg 	modified:   index.html

--- getAnimais.php
+++ getAnimais.php
@@ -4,8 +4,12 @@
     if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
     
     //Consultando banco de dados
-    $qryLista = mysqli_query($con, "SELECT ani_latitude , ani_longitude, ani_nome, raca.raca_nome FROM animal
-	INNER JOIN raca ON animal.ani_raca = raca.raca_id");    
+    $qryLista = mysqli_query($con, 
+        "SELECT ani_latitude , ani_longitude, ani_nome, raca.raca_nome,ani_imgpath,tipo_animal.tipoani_nome,ani_porte 
+        FROM animal
+    	INNER JOIN raca ON animal.ani_raca = raca.raca_id
+    	INNER JOIN tipo_animal ON tipo_animal.tipoani_id = raca.raca_tipoanimal"
+	);    
     while($resultado = mysqli_fetch_assoc($qryLista)){
         $vetor[] = array_map('utf8_encode', $resultado); 
     }    
