69b38f6e65952947ac9865a70de64e561720a9fe
fabianoaz <fabianoaz@hotmail.com>
2016-11-21 15:59:22 -0200

    Add files via upload
    Fiz alteração unificando todas funções de combos em um único arquivo JS e todas consultas também, em um único arquivo JS.
     - fnMontaCombos.js
     - Consultas.js
    
    O arquivo index.html foi alterado apenas o apontamento dos arquivos JS utilizados.

--- js/fnMontaCombos.js
+++ js/fnMontaCombos.js
@@ -5,11 +5,11 @@ function fnCombos(valueParam){
 	
 	var combo = "<option value=''>Selecione...</option>";
 	var combobox;
+	var frm;
 	
 	if(valueParam == "tag"){
 		
 		combobox = $("#combo2");
-		//combo = "<option value=''>Selecione...</option>";
 		
 		var request = $.ajax({
 			url: "php/getDadosTAG.php",
@@ -23,7 +23,7 @@ function fnCombos(valueParam){
 			}
 				combobox.append(combo);
 				combobox.show();
-				
+			
 				combobox = $("#combo3");
 				combobox.empty();
 				combobox.hide();
@@ -49,11 +49,11 @@ function fnCombos(valueParam){
 			for(i=0;i<resultado.length;i++){
 				var aux="";
 				if(resultado[i] == "P"){
-					aux = "Pequeno";
+					aux = "PEQUENO";
 				}else if(resultado[i] == "M"){
-					aux = "Médio";	
+					aux = "MÉDIO";	
 				}else if(resultado[i] == "G"){
-					aux = "Grande";
+					aux = "GRANDE";
 				}			
 				combo +="<option value="+ resultado[i] +">" + aux +"</option>";
 			}
@@ -96,21 +96,197 @@ function fnCombos(valueParam){
 				combobox = $("#combo5");
 				combobox.empty();
 				combobox.hide();
-			});				
-	}else if(valueParam == "racatag"){
+			});		
+			
+	}else if(valueParam == "mult"){
 		
-			var combo2 = $("#combo5");
-			combo2.append("<option value='valor 1'> Valor 1 </option>");
-			combo2.show();
+		console.log("Parametro MULT!!" + valueParam);
 		
-				combobox = $("#combo2");
-				combobox.empty();
-				combobox.hide();
-				combobox = $("#combo3");
-				combobox.empty();
-				combobox.hide();
-				combobox = $("#combo4");
-				combobox.empty();
-				combobox.hide();
+		frm = $("#frm6");		
+		frm.show();
+			
 	}
 }
+
+function fnCombo2(valueParam){
+	
+	var combobox = $("#combo2");
+	var frm = $("#frm2");
+	
+//--------------------------------------------------------	
+			circle.setMap(null);
+    		
+			console.log(valueParam);
+			
+			var request = $.ajax({
+				url: "php/getTag.php",
+				type: "POST",
+				data: "tag=" + valueParam,
+				dataType: "json"
+			}).done(function(dados) {
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+				alert("\nATENÇÃO:\nNenhum animal localizado!");
+			});
+//--------------------------------------------------------
+	combobox.empty();
+	combobox.hide();
+	frm.hide();
+	
+	
+	var padrao = "Selecionar por...";
+	combobox = $("#combo1");
+	combobox.val(padrao);
+}
+
+function fnCombo3(valueParam){
+	
+	var combobox = $("#combo3");
+	
+//-------------------------------------------------------------	
+            circle.setMap(null);
+    		
+			console.log(valueParam);
+			
+			var request = $.ajax({
+				url: "php/getPorte.php",
+				type: "POST",
+				data: "porte=" + valueParam,
+				dataType: "json"
+			}).done(function(dados) {
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+				alert("\nATENÇÃO:\nNenhum animal localizado!");
+			});
+//-------------------------------------------------------------	
+	combobox.hide();
+	combobox.empty();
+	
+	var padrao = "Selecionar por...";
+	combobox = $("#combo1");
+	combobox.val(padrao);
+}
+
+function fnCombo4(valueParam){
+	
+	var combobox = $("#combo4");
+	
+//-------------------------------------------------------------
+			circle.setMap(null);
+    		
+			console.log(valueParam);
+			
+			var request = $.ajax({
+				url: "php/getRaca.php",
+				type: "POST",
+				data: "raca=" + valueParam,
+				dataType: "json"
+			}).done(function(dados) {
+				
+				if(dados.length <=0){
+					alert("Dados vazios...");
+				}
+				
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+				alert("\nATENÇÃO:\nNenhum animal localizado!");
+			});
+//-------------------------------------------------------------
+	
+	combobox.hide();
+	combobox.empty();
+	
+	var padrao = "Selecionar por...";
+	combobox = $("#combo1");
+	combobox.val(padrao);
+}
+
+function fnCombo5(valueParam){
+	
+	var combobox = $("#combo5");
+	
+	alert("Selecionado: " + valueParam);
+	
+	combobox.hide();
+	combobox.empty();
+	
+	var padrao = "Selecionar por...";
+	combobox = $("#combo1");
+	combobox.val(padrao);
+}
+
+function fechaMult(){
+	var padrao = "Selecionar por...";
+	var frm = $("#frm6");
+	
+	combobox = $("#combo1");
+	combobox.val(padrao);
+	frm.hide();
+}
+
