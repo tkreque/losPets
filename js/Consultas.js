c697bdc535d094ee1fb850c8b9b5f6119a725c6f
fabianoaz <fabianoaz@hotmail.com>
2016-11-21 15:56:23 -0200

    Add files via upload

---
+++ js/Consultas.js
@@ -0,0 +1,114 @@
+function BtnCon()
+		{	
+            circle.setMap(null);
+    		
+			
+			var request = $.ajax({
+				url: "php/getAnimais.php",
+				type: "POST",
+				dataType: "json"
+			}).done(function(dados) {
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+			});
+		}
+		
+function BtnConsultaTag(tag)
+		{	
+            circle.setMap(null);
+    		
+			console.log(tag);
+			
+			var request = $.ajax({
+				url: "getTag.php",
+				type: "POST",
+				data: "tag=" + tag,
+				dataType: "json"
+			}).done(function(dados) {
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+			});
+		}
+
+function BtnConsultaPorte(porte)
+		{	
+            circle.setMap(null);
+    		
+			console.log(porte);
+			
+			var request = $.ajax({
+				url: "getPorte.php",
+				type: "POST",
+				data: "porte=" + porte,
+				dataType: "json"
+			}).done(function(dados) {
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+			});
+		}		
--- js/fnMontaCombos.js
+++ js/fnMontaCombos.js
@@ -108,6 +108,179 @@ function fnCombos(valueParam){
 	}
 }
 
+function fnCombo2(valueParam){
+	
+	var combobox = $("#combo2");
+	var frm = $("#frm2");
+	
+//--------------------------------------------------------	
+			circle.setMap(null);
+    		
+			console.log(valueParam);
+			
+			var request = $.ajax({
+				url: "php/getTag.php",
+				type: "POST",
+				data: "tag=" + valueParam,
+				dataType: "json"
+			}).done(function(dados) {
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+				alert("\nATENÇÃO:\nNenhum animal localizado!");
+			});
+//--------------------------------------------------------
+	combobox.empty();
+	combobox.hide();
+	frm.hide();
+	
+	
+	var padrao = "Selecionar por...";
+	combobox = $("#combo1");
+	combobox.val(padrao);
+}
+
+function fnCombo3(valueParam){
+	
+	var combobox = $("#combo3");
+	
+//-------------------------------------------------------------	
+            circle.setMap(null);
+    		
+			console.log(valueParam);
+			
+			var request = $.ajax({
+				url: "php/getPorte.php",
+				type: "POST",
+				data: "porte=" + valueParam,
+				dataType: "json"
+			}).done(function(dados) {
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+				alert("\nATENÇÃO:\nNenhum animal localizado!");
+			});
+//-------------------------------------------------------------	
+	combobox.hide();
+	combobox.empty();
+	
+	var padrao = "Selecionar por...";
+	combobox = $("#combo1");
+	combobox.val(padrao);
+}
+
+function fnCombo4(valueParam){
+	
+	var combobox = $("#combo4");
+	
+//-------------------------------------------------------------
+			circle.setMap(null);
+    		
+			console.log(valueParam);
+			
+			var request = $.ajax({
+				url: "php/getRaca.php",
+				type: "POST",
+				data: "raca=" + valueParam,
+				dataType: "json"
+			}).done(function(dados) {
+				
+				if(dados.length <=0){
+					alert("Dados vazios...");
+				}
+				
+				console.log(dados);
+				for (var i = 0; i < markers_on_map.length; i++ ) {
+                     markers_on_map[i].setMap(null);
+                  }
+                  markers_on_map.length = 0;
+				
+				var marker,i;
+				for(i=0;i< dados.length; i++){
+					marker = new google.maps.Marker({
+						position: new google.maps.LatLng(dados[i].ani_latitude,dados[i].ani_longitude),
+						map: map,
+						animation: google.maps.Animation.DROP,
+						title: dados[i].ani_nome,
+						infowindow: new google.maps.InfoWindow({
+                            content: "<p>Nome: "+dados[i].ani_nome+"<br />Raça: "+dados[i].raca_nome+"</p>"
+                        })
+					});
+					markers_on_map.push(marker);
+					google.maps.event.addListener(marker, 'click', function() {
+                        this.infowindow.open(map, this);
+                    }); 
+				}
+			}).fail(function(jqXHR, textStatus ) {
+				console.log("Request failed: " + textStatus);
+				alert("\nATENÇÃO:\nNenhum animal localizado!");
+			});
+//-------------------------------------------------------------
+	
+	combobox.hide();
+	combobox.empty();
+	
+	var padrao = "Selecionar por...";
+	combobox = $("#combo1");
+	combobox.val(padrao);
+}
+
+function fnCombo5(valueParam){
+	
+	var combobox = $("#combo5");
+	
+	alert("Selecionado: " + valueParam);
+	
+	combobox.hide();
+	combobox.empty();
+	
+	var padrao = "Selecionar por...";
+	combobox = $("#combo1");
+	combobox.val(padrao);
+}
+
 function fechaMult(){
 	var padrao = "Selecionar por...";
 	var frm = $("#frm6");
