e6953061adce7cb31bb0dc1f10870e803a799022
Ciro Stodulski de Azevedo <ciro.sda@gmail.com>
2016-12-03 21:49:14 -0200

    Add files via upload
    atualizando incluir e cadastro php

--- cadastro.php
+++ cadastro.php
@@ -6,14 +6,18 @@ or die("Erro na conexão com banco de dados"); //caso não consiga conectar most
 $select_db = mysql_select_db("pets"); //seleciona o banco de dados
 
 //Abaixo atribuímos os valores provenientes do formulário pelo método POST
-$nome	= 1;	//atribuição do campo "nome" vindo do formulário para variavel
-$usuario	= $_POST["usuario"];	//atribuição do campo "email" vindo do formulário para variavel
+$nome	= $_POST["nome"];	//atribuição do campo "nome" vindo do formulário para variavel
+$usuario	= 1;	//atribuição do campo "email" vindo do formulário para variavel
 $raca	= $_POST["raca"];	//atribuição do campo "ddd" vindo do formulário para variavel
 $porte	= $_POST["porte"];	//atribuição do campo "telefone" vindo do formulário para variavel
 $situacao	= $_POST ["situacao"];	//atribuição do campo "endereco" vindo do formulário para variavel
 $latitude	= $_POST ["txtLatitude"];
 $longitude	= $_POST ["txtLongitude"];
-$foto = $_FILES["foto"];
+//$tipo = $_POST['tipo'];
+//var_dump($_POST);
+//die();
+/*$foto = $_FILES["foto"];
+
 if (!empty($foto["name"])) {      
     // Largura máxima em pixels
     $largura = 150;
@@ -65,8 +69,8 @@ if (!empty($foto["name"])) {
             echo $erro . "<br />";
         }
     }
-}else{
-    $string_sql = "INSERT INTO `animal` (`ani_id`, `ani_nome`, `ani_raca`, `ani_porte`, `ani_situacao`, `ani_usuario`, 'ani_latitude', 'ani_longitude') VALUES (NULL, '$nome', '$raca', '$porte', '$situacao', '$usuario', '$latitude', '$longitude')";
+}else{*/
+    $string_sql = "INSERT INTO `pets`.`animal` (`ani_id`, `ani_nome`, `ani_raca`, `ani_porte`, `ani_situacao`, `ani_usuario`, `ani_latitude`, `ani_longitude`, `ani_imgpath`, `ani_data`) VALUES (NULL, '$nome', '$raca', '$porte', '$situacao', '$usuario', '$latitude', '$longitude', NULL, NULL)";
     mysql_query($string_sql,$conexao);
     if(mysql_affected_rows() == 1){ //verifica se foi afetada alguma linha, nesse caso inserida alguma linha
         echo "<p>Cadastro feito com sucesso</p>";
@@ -75,7 +79,7 @@ if (!empty($foto["name"])) {
         echo "Erro, não possível inserir no banco de dados";
     }
     mysql_close($conexao); //fecha conexão com banco de dados
-}
+//}
 
 
 
